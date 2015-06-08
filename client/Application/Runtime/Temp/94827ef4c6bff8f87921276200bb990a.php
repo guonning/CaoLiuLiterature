32ffa6f39a4546e4dd71f241cf0e4306e5d586c2
redzl <sarly0811@163.com>
2015-06-09 01:49:02 +0800

    remove runtime

--- client/Application/Runtime/Temp/94827ef4c6bff8f87921276200bb990a.php
+++
@@ -1,3 +0,0 @@
-<?php
-//000000003600a:2:{i:0;a:7:{s:2:"id";s:1:"1";s:11:"server_name";s:12:"IOS-TEST1服";s:10:"login_port";s:4:"7710";s:11:"recharge_ip";s:14:"192.168.37.199";s:13:"recharge_port";s:3:"810";s:4:"type";s:7:"android";s:6:"status";s:1:"1";}i:1;a:7:{s:2:"id";s:1:"2";s:11:"server_name";s:12:"IOS-TEST2服";s:10:"login_port";s:4:"7711";s:11:"recharge_ip";s:14:"192.168.37.200";s:13:"recharge_port";s:3:"810";s:4:"type";s:7:"android";s:6:"status";s:1:"1";}}
-?>
--- client/Application/Runtime/Temp/a8438da78e679f44a5cff9e44ebacfbd.php
+++
@@ -1,3 +0,0 @@
-<?php
-//000000000000a:7:{s:2:"id";s:1:"1";s:11:"server_name";s:12:"IOS-TEST1服";s:10:"login_port";s:4:"7710";s:11:"recharge_ip";s:14:"192.168.37.199";s:13:"recharge_port";s:3:"810";s:4:"type";s:7:"android";s:6:"status";s:1:"1";}
-?>
