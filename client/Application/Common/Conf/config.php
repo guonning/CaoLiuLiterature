dab9ec933a445800c0be079cb5e771ccb96fd51e
redzl <sarly0811@163.com>
2015-06-09 19:00:59 +0800

    complete client crawl

--- client/Application/Common/Conf/config.php
+++ client/Application/Common/Conf/config.php
@@ -1,6 +1,7 @@
 <?php
 return array(
 	//数据库配置
+	'SITE_NAME' => '文学手机站',
     'DB_TYPE' => 'mysqli',
 	'DB_HOST' => '127.0.0.1',
 	'DB_NAME' => 'xzns',
@@ -8,6 +9,15 @@ return array(
 	'DB_PWD' => 'dashabi123!',
 	'DB_PORT'  => '3306',
 	
-	'MODULE_ALLOW_LIST'    => array('Home'),
+	'TMPL_PARSE_STRING' => array(
+		'__CSS__' => '/static/'.THEME_ID.'/css',
+        '__JS__' => '/static/'.THEME_ID.'/js',
+        '__IMG__' => '/static/'.THEME_ID.'/images',
+	),
+	'VIEW_PATH' => APP_PATH . '../Theme/' . THEME_ID .'/',
+    'LAYOUT_ON'	=> false,
+    'LAYOUT_NAME'	=> 'layout',
+    'MODULE_ALLOW_LIST'    => array('Home','Manager'),
     'DEFAULT_MODULE' => 'Home',
+    'PAGESIZE'  => 15,
 );
--- client/Application/Home/Controller/IndexController.class.php
+++ client/Application/Home/Controller/IndexController.class.php
@@ -4,6 +4,36 @@ use Think\Controller;
 
 class IndexController extends Controller {
     public function index(){
-        $this->show("<h1>It's works!</h1>");
+        $this->display();
     }    
+
+    public function ajax() {
+	    $page = I('get.page', 1);
+	    $content = $this->_get('http://localhost:8081/caoliu.php?act=index&page=' . $page);
+	    $data = array(
+			'pages'		=> 5,
+			'records'	=> json_decode($content, true),
+			'next_page'	=> $page + 1
+	    );
+
+	    $this->ajaxReturn($data);
+    }
+
+    public function detail($url) {
+	    $content = $this->_get('http://localhost:8081/caoliu.php?act=detail&url=' . $url);
+	    $content = json_decode($content, true);
+	    $this->assign('data', $content);
+	    $this->display();
+    }
+
+    private function _get($url) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Baiduspider+(+http://www.baidu.com/search/spider.htm)');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $info = curl_exec($ch);
+        curl_close($ch);
+        return $info;
+    }
 }
